9f3577191e8bef1ff24626498f6feaa9285aa403
Jose Mario <josemariog99@gmail.com>
2022-01-30 15:27:21 -0500

    agregando funcionalidad a login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,111 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ButtonForm, CampoDiv, InputForm, LoginDiv, LogoDiv } from '../styles/LoginStyles';
+import Swal from 'sweetalert2'
+import axios from 'axios';
 
-const Login = () => {
-  return (
-    <LoginDiv>
-        <LogoDiv>
-            <img src="https://res.cloudinary.com/karma09-jm/image/upload/v1643347552/Guappjolotas/logo_gndgre.svg" alt="Logo" width={150}/>
-        </LogoDiv>
-        <form action="">
-            <CampoDiv>
-                <i className="bi bi-person"></i>
-                <InputForm type="text" name="usuario" placeholder='Usuario' required />
-            </CampoDiv>
-            <CampoDiv>
-                <i class="bi bi-lock"></i>
-                <InputForm type="password" name="password" placeholder='Contraseña' required />
-            </CampoDiv>
-            <ButtonForm type="submit">Iniciar Sesión</ButtonForm>
-        </form>
-    </LoginDiv>
-  );
+export default class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            form: {
+                nombre_usuario: "",
+                contraseña: ""
+            }
+        }
+    }
+
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.IniciarSesion();
+    }
+
+    handleInputChange =  ({target}) => {
+        this.setState({
+            form: {
+                ...this.state.form,
+                [target.name]: target.value
+            }
+        });
+    }
+
+    IniciarSesion = async () => {
+        await axios.get('https://guappjolotas-users.herokuapp.com/usuarios/', {
+            params: {
+                nombre_usuario: this.state.form.nombre_usuario,
+                contraseña: this.state.form.contraseña
+            }
+        })
+        .then(resp => {
+            const usuario = resp.data
+            const usuarioIsReal = usuario.find((user) => user.nombre_usuario === this.state.form.nombre_usuario)
+            console.log(usuarioIsReal);
+
+            if (usuarioIsReal === undefined) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'El usuario no existe!',
+                  })
+            }
+            else if (usuarioIsReal !== "") {
+                if (usuarioIsReal.contraseña === this.state.form.contraseña) {
+                    localStorage.setItem('Usuario', usuarioIsReal.nombre_usuario)
+                    window.location.href = "/home"
+
+                }
+                else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'contraseña incorrecta!',
+                      })
+                }
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Hubo un error!',
+              })
+        })
+    }
+
+    render(){
+        return (
+          <LoginDiv>
+              <LogoDiv>
+                  <img src="https://res.cloudinary.com/karma09-jm/image/upload/v1643347552/Guappjolotas/logo_gndgre.svg" alt="Logo" width={150}/>
+              </LogoDiv>
+              <form onSubmit={this.handleSubmit}>
+                  <CampoDiv>
+                      <i className="bi bi-person"></i>
+                      <InputForm 
+                          type="text" 
+                          name="nombre_usuario" 
+                          placeholder='Usuario' 
+                          value={this.state.form.nombre_usuario} 
+                          onChange={this.handleInputChange} 
+                          required 
+                      />
+                  </CampoDiv>
+                  <CampoDiv>
+                      <i className="bi bi-lock"></i>
+                      <InputForm 
+                          type="password" 
+                          name="contraseña" 
+                          placeholder='Contraseña' 
+                          value={this.state.form.contraseña} 
+                          onChange={this.handleInputChange} 
+                          required 
+                      />
+                  </CampoDiv>
+                  <ButtonForm type="submit">Iniciar Sesión</ButtonForm>
+              </form>
+          </LoginDiv>
+        );
+
+    }
 };
-
-export default Login;
